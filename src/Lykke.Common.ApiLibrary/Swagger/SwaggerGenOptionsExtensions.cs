04b54b506837bf71df0aca804bbafcb6e1e9f0f6
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-06-21 15:26:11 +0300

    Default swagger configuration added

--- src/Lykke.Common.ApiLibrary/Swagger/SwaggerGenOptionsExtensions.cs
+++ src/Lykke.Common.ApiLibrary/Swagger/SwaggerGenOptionsExtensions.cs
@@ -1,3 +1,7 @@
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.Swagger.Model;
 using Swashbuckle.SwaggerGen.Application;
 
 namespace Lykke.Common.ApiLibrary.Swagger
@@ -12,6 +16,36 @@ namespace Lykke.Common.ApiLibrary.Swagger
         public static void EnableXmsEnumExtension(this SwaggerGenOptions swaggerOptions, XmsEnumExtensionsOptions options = XmsEnumExtensionsOptions.UseEnums)
         {
             swaggerOptions.SchemaFilter<XmsEnumSchemaFilter>(options);
+        }
+
+        /// <summary>
+        /// Setups swagger
+        /// </summary>
+        /// <param name="swaggerOptions"></param>
+        /// <param name="apiVersion">Api version. e.g: "v1"</param>
+        /// <param name="apiTitle">Api title</param>
+        public static void DefaultLykkeConfiguration(this SwaggerGenOptions swaggerOptions, string apiVersion, string apiTitle)
+        {
+            swaggerOptions.SingleApiVersion(new Info
+            {
+                Version = apiVersion,
+                Title = apiTitle
+            });
+
+            swaggerOptions.DescribeAllEnumsAsStrings();
+            swaggerOptions.EnableXmsEnumExtension();
+
+            //Determine base path for the application.
+            var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+            var entryAssembly = Assembly.GetEntryAssembly();
+
+            //Set the comments path for the swagger json and ui.
+            var xmlPath = Path.Combine(basePath, $"{entryAssembly.GetName().Name}.xml");
+
+            if (File.Exists(xmlPath))
+            {
+                swaggerOptions.IncludeXmlComments(xmlPath);
+            }
         }
     }
 }
