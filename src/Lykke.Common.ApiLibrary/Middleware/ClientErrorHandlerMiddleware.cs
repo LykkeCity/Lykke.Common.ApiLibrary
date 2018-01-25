64e4f626bd9a441ef0d4ff17bfa80328cd7231d6
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-01-25 14:49:40 +0300

    Added middleware for logging client errors

---
+++ src/Lykke.Common.ApiLibrary/Middleware/ClientErrorHandlerMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Common.Log;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace Lykke.Common.ApiLibrary.Middleware
+{
+    public class ClientErrorHandlerMiddleware
+    {
+        private readonly ILog _log;
+        private readonly string _componentName;
+        private readonly RequestDelegate _next;
+
+        public ClientErrorHandlerMiddleware(RequestDelegate next, ILog log, string componentName)
+        {
+            _log = log ?? throw new ArgumentNullException(nameof(log));
+            _componentName = componentName ?? throw new ArgumentNullException(nameof(componentName));
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next.Invoke(context);
+
+            if (context.Response.StatusCode >= StatusCodes.Status400BadRequest && 
+                context.Response.StatusCode <= StatusCodes.Status451UnavailableForLegalReasons)
+            {
+                await LogWarning(context);
+            }
+        }
+
+        private async Task LogWarning(HttpContext context)
+        {
+            var body = string.Empty;
+
+            if (context.Request.Body.CanSeek)
+            {
+                context.Request.Body.Seek(0, SeekOrigin.Begin);
+
+                // max Azure table column size is 64k
+                var size = 64 * 1024; 
+                var data = await context.Request.Body.ReadAsMuchAsPossible(size);
+
+                body = Encoding.UTF8.GetString(data);
+            }
+
+            await _log.WriteWarningAsync(_componentName, context.Request.GetUri().AbsoluteUri, body,
+                $"Client error {context.Response.StatusCode}");
+        }
+    }
+}
--- src/Lykke.Common.ApiLibrary/Middleware/MiddlewareApplicationBuilderExtensions.cs
+++ src/Lykke.Common.ApiLibrary/Middleware/MiddlewareApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Internal;
 
 namespace Lykke.Common.ApiLibrary.Middleware
 {
@@ -10,9 +11,23 @@ namespace Lykke.Common.ApiLibrary.Middleware
         /// <param name="app">Application builder</param>
         /// <param name="componentName">Component name for logs</param>
         /// <param name="createGlobalErrorResponse">Create global error response delegate</param>
-        public static void UseLykkeMiddleware(this IApplicationBuilder app, string componentName, CreateErrorResponse createGlobalErrorResponse)
+        public static void UseLykkeMiddleware(this IApplicationBuilder app, string componentName, CreateErrorResponse createGlobalErrorResponse,
+            bool logClientErrors = false)
         {
+            app.Use(async (context, next) =>
+            {
+                // enable ability to seek on request stream within any host,
+                // but not only Kestrel, for any subsequent middleware
+                context.Request.EnableRewind();
+                await next();
+            });
+
             app.UseMiddleware<GlobalErrorHandlerMiddleware>(componentName, createGlobalErrorResponse);
+
+            if (logClientErrors)
+            {
+                app.UseMiddleware<ClientErrorHandlerMiddleware>(componentName);
+            }
         }
     }
 }
