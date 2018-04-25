2ac7da4904cbfa4e37d6094669b89a875245861f
Zhdanovich Pavel <swatoplus@gmail.com>
2018-04-25 14:17:07 +0300

    Added FileUploadOperationFilter

---
+++ src/Lykke.Common.ApiLibrary/Swagger/FileUploadOperationFilter.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Lykke.Common.ApiLibrary.Swagger
+{
+    public class FileUploadOperationFilter : IOperationFilter
+    {
+        private const string MultipartFormDataMimeType = "multipart/form-data";
+
+        private static readonly string[] FormFilePropertyNames = typeof(IFormFile)
+            .GetTypeInfo()
+            .DeclaredProperties
+            .Select(x => x.Name)
+            .ToArray();
+
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            var fileApiParameters = context
+                .ApiDescription
+                .ActionDescriptor
+                .Parameters
+                .Where(x => x.ParameterType == typeof(IFormFile))
+                .ToArray();
+
+            if (fileApiParameters.Any())
+            {
+                if (!operation.Consumes.Contains(MultipartFormDataMimeType))
+                {
+                    operation.Consumes.Add(MultipartFormDataMimeType);
+                }
+            }
+
+            foreach (var fileApiParameter in fileApiParameters)
+            {
+                var fileParameter = new NonBodyParameter
+                {
+                    Name = fileApiParameter.Name,
+                    In = "formData",
+                    Required = false,
+                    Type = "file"
+                };
+
+                var firstNestedFileParameter = operation
+                    .Parameters
+                    .OfType<NonBodyParameter>()
+                    .First(x => FormFilePropertyNames.Contains(x.Name));
+
+                var fileParameterIndex = operation
+                    .Parameters
+                    .IndexOf(firstNestedFileParameter);
+
+                foreach (var propertyName in FormFilePropertyNames)
+                {
+                    var parameter = operation.Parameters.First(x => x.Name.Contains(propertyName));
+                    operation.Parameters.Remove(parameter);
+                }
+
+                operation
+                    .Parameters
+                    .Insert(fileParameterIndex, fileParameter);
+            }
+        }
+    }
+}
