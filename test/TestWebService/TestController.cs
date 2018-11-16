1956141b582624be9011efeab5600b30253691af
tarurar <tarutin_andrew@mail.ru>
2018-11-16 14:21:16 +0300

    swashbuckle updated

--- test/TestWebService/TestController.cs
+++ test/TestWebService/TestController.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace TestWebService
 {
