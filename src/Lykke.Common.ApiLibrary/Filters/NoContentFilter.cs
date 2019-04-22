254edca6446ae9985c3ed7b986cd3677598f3341
Sergey Nesterov <sergey.nesterov@lykke.com>
2019-04-22 14:39:47 +0300

    Added a filter for no content responses

---
+++ src/Lykke.Common.ApiLibrary/Filters/NoContentFilter.cs
@@ -0,0 +1,38 @@
+﻿using Common.Log;
+using JetBrains.Annotations;
+using Lykke.Common.Log;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lykke.Common.ApiLibrary.Filters
+{
+    /// <summary>
+    /// Middleware that handles responses with no content.
+    /// </summary>
+    [PublicAPI]
+    public class NoContentFilter : IResultFilter
+    {
+        private readonly ILog _log;
+
+        public NoContentFilter(ILogFactory logFactory)
+        {
+            _log = logFactory.CreateLog(this);
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            var response = context.HttpContext.Response;
+            if (response.StatusCode == StatusCodes.Status200OK && (response.ContentLength ?? 0) == 0)
+            {
+                if (response.HasStarted)
+                    _log.Warning($"Couldn't set NoContent into response to {context.HttpContext.Request.Method} {context.HttpContext.Request.Path}");
+                else
+                    response.StatusCode = StatusCodes.Status204NoContent;
+            }
+        }
+    }
+}
